98f371f322ea095a0e1f0fdf2231a524dc2c28a5
Omar Khaled <150465142+OmarKhaled-00@users.noreply.github.com>
2025-06-09 19:25:01 +0300

    Delete Safety Monitoring and Alarm System/Code/Master MCU directory

--- Safety Monitoring and Alarm System/Code/Master MCU/GIE_reg.h
+++
@@ -1,9 +0,0 @@
-#ifndef GIE_REG_H
-#define GIE_REG_H
-
- 
-#define SREG *((Volatile uint8*)0x5F)  /*Status Register*/
-#define SREG_I   7u                    /*Global interrupt enable PIN*/
-
-
-#endif
